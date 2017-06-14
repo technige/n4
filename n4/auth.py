2e52f47c1ab51bdb4f5f6618467485d3122d1fac
Nigel Small <nigel@neo4j.com>
2017-06-14 09:18:31 +0100

    Cleaned up n4a

--- n4/auth.py
+++ n4/auth.py
@@ -56,9 +56,15 @@ class AuthUser(object):
         hash_algorithm, digest, salt = fields[1].split(b",")
         return cls(name, hash_algorithm, unhex_bytes(digest), unhex_bytes(salt))
 
-    def dump(self):
-        return (b"%s:%s,%s,%s:" %
-                (self.name, self.hash_algorithm, hex_bytes(self.digest), hex_bytes(self.salt)))
+    @classmethod
+    def match(cls, s, user_name):
+        assert isinstance(s, (bytes, bytearray))
+        candidate_user_name, _, _ = s.partition(b":")
+        return candidate_user_name == user_name
+
+    def dump(self, eol=b"\r\n"):
+        return (b"%s:%s,%s,%s:%s" %
+                (self.name, self.hash_algorithm, hex_bytes(self.digest), hex_bytes(self.salt), eol))
 
     def __init__(self, name, hash_algorithm, digest, salt):
         assert hash_algorithm == b"SHA-256"
@@ -93,30 +99,100 @@ class AuthFile(object):
         self.name = name
 
     def __iter__(self):
-        try:
-            with open(self.name, "rb") as f:
-                for line in f:
-                    yield AuthUser.load(line)
-        except IOError:
-            pass
+        with open(self.name, "rb") as f:
+            for line in f:
+                yield AuthUser.load(line)
 
     def append(self, user_name, password):
         line = AuthUser.create(user_name, password).dump()
         if self.name == "-":
-            print(line.decode("utf-8"))
+            click.echo(line.decode("utf-8"), nl=False)
         else:
             with open(self.name, "ab") as f:
                 f.write(line)
-                f.write(b"\r\n")
+
+    def remove(self, user_name):
+        with open(self.name, "rb") as f:
+            lines = [line for line in f.readlines() if not AuthUser.match(line, user_name)]
+        with open(self.name, "wb") as f:
+            f.writelines(lines)
+
+    def update(self, user_name, password):
+        with open(self.name, "rb") as f:
+            lines = []
+            for line in f.readlines():
+                if AuthUser.match(line, user_name):
+                    lines.append(AuthUser.create(user_name, password).dump())
+                else:
+                    lines.append(line)
+        with open(self.name, "wb") as f:
+            f.writelines(lines)
 
 
-@click.command()
+@click.group(help="""\
+Tool for managing Neo4j auth files.
+""")
+def cli():
+    pass
+
+
+@cli.command(help="""\
+List users in the Neo4j auth file.
+""")
+@click.argument("auth_file")
+def list(auth_file):
+    for user in AuthFile(auth_file):
+        click.echo(user.name)
+
+
+@cli.command(help="""\
+Add a user to the Neo4j auth file.
+
+For general interactive use, password and confirmation prompts will be presented.
+For batch mode, use the --password option.
+
+Example:
+
+    n4a add data/dbms/auth alice
+
+If AUTH_FILE contains only a dash `-` then the auth file entry will be written to stdout instead.
+""")
 @click.argument("auth_file")
 @click.argument("user_name")
 @click.password_option()
-def main(auth_file, user_name, password):
+def add(auth_file, user_name, password):
+    AuthFile(auth_file).append(user_name, password)
+
+
+@cli.command(help="""\
+Remove a user from the Neo4j auth file.
+""")
+@click.argument("auth_file")
+@click.argument("user_name")
+def remove(auth_file, user_name):
+    AuthFile(auth_file).remove(user_name)
+
+
+@cli.command(help="""\
+Set the password for a user in the Neo4j auth file.
+
+For general interactive use, password and confirmation prompts will be presented.
+For batch mode, use the --password option.
+
+Example:
+
+    n4a update data/dbms/auth alice
+""")
+@click.argument("auth_file")
+@click.argument("user_name")
+@click.password_option()
+def update(auth_file, user_name, password):
+    AuthFile(auth_file).update(user_name, password)
+
+
+def main():
     try:
-        AuthFile(auth_file).append(user_name, password)
+        cli(obj={})
     except Exception as error:
         click.secho(error.args[0], err=True)
         exit(1)
